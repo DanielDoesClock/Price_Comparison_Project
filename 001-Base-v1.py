f29029a490a2799447baddd52b0da804a96e13f5
DanielDoesClock <83557835+DanielDoesClock@users.noreply.github.com>
2022-05-05 09:59:33 +1200

    base

---
+++ 001-Base-v1.py
@@ -0,0 +1,42 @@
+"""This is my Base for my Comparison Project.
+This will hold my plans for the project.
+Each component will be made in it's own file
+and when completed added into this one.
+When all parts are added it will create the final project file.
+Made by Daniel Fraser
+5/5/22"""
+
+# Import statements
+
+# Functions
+
+# Function for finding out which item is the best for its amount
+
+# Function for working out which is the best option based on budget
+
+# *** Main Routine ***
+
+# Lists to hold data
+
+# Variables and constants
+
+# Introduction and instructions
+
+# Ask for user's budget
+
+# Loop for items
+
+    # Ask for item name
+
+    # Ask for item price
+
+    # Ask for item amount
+
+    # Put values into a list
+
+# Tell user which is the best for it's weight
+
+# Tell user the recommended item based on price, amount and budget
+
+
+
---
+++ 002-User-Budget-v1.py
@@ -0,0 +1,2 @@
+"""This is v1 of the first component of my Price Comparison Project
+It will ask the user """
